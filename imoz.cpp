3f0856e4f711f25b45af824420e4aaff86f57042
Koki Nabeta <67694032+tigeta@users.noreply.github.com>
2023-10-26 00:10:53 +0900

    Add files via upload

---
+++ imoz.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int N, Q;
+	cin >> N >> Q;
+	int ans = 0;
+	vector<int> vec(N, 0);
+	for (int i = 0;i < Q;++i) {
+		int l, r;
+		cin >> l >> r;
+		l--; r--;
+
+		vec.at(l)++;
+		vec.at(r + 1)--;
+	}
+
+	for (int i = 1;i < N;++i) {
+		vec.at(i) += vec.at(i - 1);
+		ans = max(ans, vec.at(i));
+	}
+
+	cout << ans << endl;
+	return 0;
+}
+
