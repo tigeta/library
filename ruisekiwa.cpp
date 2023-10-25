7eddfc2ad3374c1e701aa0bc3d595ad7476af20d
Koki Nabeta <67694032+tigeta@users.noreply.github.com>
2023-10-25 17:37:51 +0900

    Add files via upload

---
+++ ruisekiwa.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int N, X, Y;
+	cin >> N >> X >> Y;
+	vector<int> sum(N + 1, 0);
+
+
+	for (int i = 0;i < N;++i) {
+		int a; cin >> a;
+		sum.at(i + 1) = sum.at(i) + a;
+	}
+
+	cout << sum.at(Y + 1) - sum.at(X) << endl;
+}
+
