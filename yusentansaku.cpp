95323cf2a7baeb730d70e5454339d36d5432620a
Koki Nabeta <67694032+tigeta@users.noreply.github.com>
2023-10-30 21:04:26 +0900

    Update yusentansaku.cpp

--- yusentansaku.cpp
+++ yusentansaku.cpp
@@ -5,54 +5,51 @@
 
 using namespace std;
 
+vector <int> movey = { 0,0,1,-1,1,-1,1,-1 }; //隣接マスを調べるためのy,x方向の配列
+vector <int> movex = { 1,-1,0,0,1,1,-1,-1 };
+
 int H, W;
 
 
-int dfs(int y, int x, vector<vector<bool>> &v) {
-	v.at(y).at(x) = true;
-		if (y - 1 >= 0 && !v.at(y-1).at(x)){
-			dfs(y - 1, x,v);
+void dfs(int y, int x, vector<vector<bool>> &f) { //再帰で記述した深さ優先探索
+	f.at(y).at(x) = true;
+		if (y - 1 >= 0 && !f.at(y-1).at(x)){
+			dfs(y - 1, x,f);
 		}
-		if (x + 1 < W && !v.at(y).at(x+1)) {
-			dfs(y , x+1,v);
+		if (x + 1 < W && !f.at(y).at(x+1)) {
+			dfs(y , x+1,f);
 		}
-		if (y + 1 < H && !v.at(y + 1).at(x)) {
-			dfs(y + 1, x,v);
+		if (y + 1 < H && !f.at(y + 1).at(x)) {
+			dfs(y + 1, x,f);
 		}
-		if (x - 1 >= 0 && !v.at(y).at(x-1)) {
-			dfs(y , x-1,v);
+		if (x - 1 >= 0 && !f.at(y).at(x-1)) {
+			dfs(y , x-1,f);
 		}
 
-		return 1;
 }
 
-int main()
-{
-	int  Y, X;
-	cin >> H >> W;
-	int ans = 0;
-	vector<string> maze(H);
-	vector<vector<bool>> flag(H, vector<bool>(W, false));
-	for (int i = 0;i < H;++i) {
-		cin >> maze.at(i);
-	}
+void bfs(vector<vector<bool>> &f,vector<string> &g, int y, int x) { //幅優先探索(1度調べたかを保存する配列ｆと迷路の配列g)
+	queue <int> qy;
+	queue <int> qx;
+	qy.push(y);
+	qx.push(x);
 
-	for (int i = 0;i < H;++i) {
-		for (int j = 0;j < W;++j) {
-			if (maze.at(i).at(j) == '#') flag.at(i).at(j) = true;
-		}
-	}
-	
+	while (!qy.empty()) { //queueの中身が無くなるまで探索
+		
+			for (int i = 0;i < 8;++i) {
+				int y = qy.front() + movey.at(i);
+				int x = qx.front() + movex.at(i);
 
-	for (int i = 0;i < H;++i) {
-		for (int j = 0;j < W;++j) {
-			if (!flag.at(i).at(j)) {
-				ans += dfs(i, j, flag);
+				if (x >= 0 && y >= 0 && x < W && y < H && !f.at(y).at(x)) {
+					if (g.at(y).at(x) == '#') {
+						f.at(y).at(x) = true;
+						qy.push(y);
+						qx.push(x);
+					}
+				}
 			}
-		}
+			qy.pop();
+			qx.pop();
 	}
 
-	cout << ans << endl;
-	return 0;
 }
-
