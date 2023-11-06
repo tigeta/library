be83366ad9f94f3f3abcfb453affd181b555a1d4
Koki Nabeta <67694032+tigeta@users.noreply.github.com>
2023-11-06 20:30:31 +0900

    Add files via upload

---
+++ dp.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n, x, a, y, b, z, c;
+    cin >> n >> x >> a >> y >> b >> z >> c;
+
+    vector<int> dp(2000,100000000);
+    dp.at(0) = 0;
+    dp.at(x) = a;
+
+    for (int i = 1;i < n+z;++i) {
+        if(i >= x) {
+            dp.at(i) = min(dp.at(i), dp.at(i - x) + a);
+        }
+        if (i >= y) {
+            dp.at(i) = min(dp.at(i), dp.at(i - y) + b);
+        }
+        if (i >= z) {
+            dp.at(i) = min(dp.at(i), dp.at(i - z) + c);
+        }
+    }
+
+    for (int i = n + z - 1;i > 0;i--) {
+        dp.at(i) = min(dp.at(i), dp.at(i + 1));
+    }
+
+    cout << dp.at(n) << endl;
+    return 0;
+}
+
