2ba0f5003d321afbec80218c3a856bd52229553b
Koki Nabeta <67694032+tigeta@users.noreply.github.com>
2023-11-07 18:25:32 +0900

    Add files via upload

---
+++ dp_lis .cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    vector<int> vec(n);
+    vector<int> dp(n);
+
+    cin >> vec.at(0);
+  
+    for (int i = 1;i < n;++i) {
+        cin >> vec.at(i);
+    }
+
+    for (int i = 0;i < n;++i) {
+        dp.at(i) = 1;
+        for (int j = 0;j < i;++j) {
+            if (vec.at(j) > vec.at(i)) {
+                dp.at(i) = max(dp.at(i), dp.at(j) + 1);
+            }
+        }
+    }
+
+
+    cout << *max_element(dp.begin(),dp.end()) << endl;
+    return 0;
+}
+
