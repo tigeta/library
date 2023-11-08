d9e2bfb0259eaae24d3c579a6da760f7ba4c73ed
Koki Nabeta <67694032+tigeta@users.noreply.github.com>
2023-11-09 01:40:50 +0900

    Add files via upload

---
+++ dp _bubunnwa.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+    int n, x;
+    cin >> n >> x;
+   
+    vector<int> vec(n);
+    vector<int> dp(x+1,100000000);
+
+    dp.at(0) = 0;
+    for (int i = 0;i < n;++i) {
+        cin >> vec.at(i);
+    }
+
+    
+        for (int i = 0;i < n;++i) {
+            for (int j = vec.at(i);j <= x;j++) {
+                if (dp.at(j - vec.at(i)) != 100000000) {
+                    dp.at(j) = min(dp.at(j - vec.at(i)) + 1, dp.at(j));
+                }
+            }
+        }
+    
+   
+
+    if (dp.at(x) == 100000000) {
+        cout << "no" << endl;
+    }
+    else {
+        cout << "yes" << endl;
+    }
+
+    return 0;
+}
+
